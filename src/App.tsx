82b4a373e90efd893c603fd76caca80f08b4b3fb
FR <83808214+fukasawaryosuke@users.noreply.github.com>
2023-08-31 17:35:43 +0900

    Setting up React with TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function App() {
+//   return <div className="App"></div>;
+// }
+
+const App: React.FC = () => {
+  return <div className="App"></div>;
+};
+
+export default App;
